4578378201be0cf7c555311565d494f1518199ae
Nalaka Dinesh <nalakadinesh123@gmail.com>
2025-07-12 23:22:10 +0300

    forgot this

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,24 +1,56 @@
 import Image1 from '../../assets/testimonial1.png';
 import Image2 from '../../assets/testimonial2.png';
 import Image3 from '../../assets/testimonial3.png';
+import Image4 from '../../assets/t1.png';
+import Image5 from '../../assets/t2.jpg';
+import Image6 from '../../assets/t3.jpg';
+import Image7 from '../../assets/t4.jpg';
 
 export const Data = [
   {
     id: 1,
+    image: Image4,
+    title: "Heiman KenZhari",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 2,
+    image: Image5,
+    title: "Gerhard",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 3,
+    image: Image6,
+    title: "Anuradha Thabrew",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 4,
+    image: Image7,
+    title: "Kasun Hasanka",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 5,
     image: Image1,
     title: "Jhon Doe",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
   },
   {
-    id: 2,
+    id: 6,
     image: Image2,
     title: "Harry Clinton",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
   },
   {
-    id: 3,
+    id: 7,
     image: Image3,
     title: "Sara Cill",
     description:
