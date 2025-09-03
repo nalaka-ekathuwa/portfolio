097c38b52badada8e41f01f64a1a7130041934da
Nalaka Dinesh <nalakadinesh123@gmail.com>
2025-09-03 17:54:18 +0300

    link_fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,30 +9,30 @@ const Footer = () => {
 
             <ul className="footer__list">
                 <li>
-                    <a href="#about" className="footer__link">About</a>
+                    <a href="#about" rel="noopener noreferrer" className="footer__link">About</a>
                 </li>
                 <li>
-                    <a href="#skills" className="footer__link">Skills</a>
+                    <a href="#skills" rel="noopener noreferrer" className="footer__link">Skills</a>
                 </li>
                 <li>
-                    <a href="#services" className="footer__link">Projects</a>
+                    <a href="#services" rel="noopener noreferrer" className="footer__link">Projects</a>
                 </li>
                 <li>
-                    <a href="#qualification" className="footer__link">Qualifications</a>
+                    <a href="#qualification" rel="noopener noreferrer" className="footer__link">Qualifications</a>
                 </li>
                 <li>
-                    <a href="#testimonials" className="footer__link">Testimonials</a>
+                    <a href="#testimonials" rel="noopener noreferrer" className="footer__link">Testimonials</a>
                 </li>
             </ul>
 
             <div className="footer__social">
-                <a href="https://www.instagram.com/nalaka_nd/" className="footer__social-link" target='_blank' >
+                <a href="https://www.instagram.com/nalaka_nd/" rel="noopener noreferrer" className="footer__social-link" target='_blank' >
                     <i className="bx bxl-instagram "></i>
                 </a>
-                <a href="https://www.facebook.com/nalaka.dinesh.54" className="footer__social-link" target='_blank' >
+                <a href="https://www.facebook.com/nalaka.dinesh.54" rel="noopener noreferrer" className="footer__social-link" target='_blank' >
                     <i className="bx bxl-facebook-square "></i>
                 </a>
-                <a href="https://www.linkedin.com/in/nalakaruberu/" className="footer__social-link" target='_blank' >
+                <a href="https://www.linkedin.com/in/nalakaruberu/" rel="noopener noreferrer" className="footer__social-link" target='_blank' >
                     <i className="bx bxl-linkedin-square "></i>
                 </a>
             </div>
